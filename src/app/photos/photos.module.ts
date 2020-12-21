12f2908bbd1869ff92daba7b20e7213ac6fdde03
laurivan <laurivansareta@gmail.com>
2020-12-21 16:41:53 -0300

    Aula 04.02

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,11 +1,15 @@
-import { HttpClientModule } from "@angular/common/http";
-import { NgModule } from "@angular/core";
-import { PhotoComponent } from "./photo/photo.component";
+import { NgModule } from '@angular/core';
+
+import { PhotoComponent } from './photo/photo.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
-    declarations: [ PhotoComponent ],
-    exports: [ PhotoComponent ],
+    declarations: [ 
+        PhotoComponent, 
+        PhotoListComponent 
+    ],
     imports: [ HttpClientModule]
 })
 export class PhotosModule {}
