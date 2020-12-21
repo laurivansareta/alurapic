dbb4c5c6d5b11c9432f88b1b01807d1b54b87655
laurivan <laurivansareta@gmail.com>
2020-12-21 09:31:53 -0300

    Aula 03.05 e 07

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,15 +9,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'alurapic';
 
-  photos = [
-    {
-      url: 'https://thumbs.jusbr.com/imgs.jusbr.com/publications/artigos/144922528/images/1412967253.jpeg',
-      description: 'Leão'
-    },
-    {
-      url: 'https://i0.statig.com.br/bancodeimagens/a8/77/le/a877lepxh7xa3woi9vt16b741.jpg',
-      description: 'Leoa'
-    }
-  ]
+  photos: object[] = [];
+
+  constructor(http: HttpClient){
+    http
+      .get<object[]>('http://localhost:3000/flavio/photos')
+      .subscribe(photos => this.photos = photos);
+  }
+  
 
 }
