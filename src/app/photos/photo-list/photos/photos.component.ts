39f344114078201a8922da656c066dc043fbcab0
laurivan <laurivansareta@gmail.com>
2020-12-22 08:56:13 -0300

    Aula 05.02 a 04

---
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Photo } from '../../photo/photo';
+
+@Component({
+  selector: 'ap-photos',
+  templateUrl: './photos.component.html',
+  styleUrls: ['./photos.component.css']
+})
+export class PhotosComponent implements OnInit {
+
+  @Input() photos: Photo[] = [];
+  rows: any[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.rows = this.groupColumns(this.photos);
+  }
+
+  groupColumns(photos: Photo[]){
+    const newRows = [];
+
+    for (let index = 0; index < this.photos.length; index+=3) {
+      newRows.push(photos.slice(index, index + 3));      
+    }
+    console.log(newRows);
+    console.log(this.photos);
+    return newRows;
+  }
+
+}
